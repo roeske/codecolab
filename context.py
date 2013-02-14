506951e085bb87a2d8ea853d245896ad7a8b6aee
Tom Dignan <thomas@toptal.com>
2013-02-14 10:02:06 -0500

    commit latest context changes (for heroku database deploy)

--- context.py
+++ context.py
@@ -1,10 +1,16 @@
 import flask
+import os
 
 # create application
 app = flask.Flask(__name__)
 
 # configure database connection
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://tom:fender@localhost:5432/codecolab"
+# local
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://tom:fender@localhost:5432/codecolab"
+
+# heroku
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
+
 
 # secret!
 app.secret_key = "8d312c5f-361e-431a-b0f5-c1890444a519"
