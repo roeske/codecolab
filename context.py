930058e2e003bf6116e078c0f54959c44b6a1c67
Tom Dignan <thomas@toptal.com>
2013-03-02 08:19:38 -0500

    Moved configuration out of context.py and rely on environ

--- context.py
+++ context.py
@@ -4,13 +4,8 @@ import os
 # create application
 app = flask.Flask(__name__)
 
-# configure database connection
-# local
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://tom:fender@localhost:5432/codecolab"
-
 # heroku
-#app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
-
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DATABASE_URL"]
 
 # secret!
 app.secret_key = "8d312c5f-361e-431a-b0f5-c1890444a519"
