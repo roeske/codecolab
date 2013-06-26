bb144252abd94f96ae4479c5b869c597b48c3c1d
Tom Dignan <thomas@toptal.com>
2013-06-25 22:02:41 -0400

    Reports reworking:
    * Make side by side again.
    * Make expand/collapsable.

--- static/js/reports_page.js
+++ static/js/reports_page.js
@@ -13,6 +13,7 @@
         ReportsPage.prototype.is_team_reports_shown = true;
         ReportsPage.prototype.tab_create_report = null;
         ReportsPage.prototype.tab_team_reports = null;
+        ReportsPage.prototype.reports_accordion = null;
 
         function ReportsPage(options) { 
             // copy parameters
@@ -25,9 +26,16 @@
             // Reference tabs.
             this.tab_create_report = require("#tab_create_report");
             this.tab_team_reports = require("#tab_team_reports");
-         
+            this.reports_accordion = require(".reports_accordion");
+
             // Reference outer scope.
             var that = this;
+
+            // Setup accordion
+            this.reports_accordion.accordion({
+                header: '.report_header',
+                heightStyle: 'content'
+            });
 
             // If the team reports tab is clicked, but the section is not
             // already shown, show it.
@@ -46,12 +54,26 @@
             });
 
             // Make sure initial state reflects what is really shown.
+            /*
             if (this.is_team_reports_shown) {
                 this.show_team_reports();
             } else {
                 this.show_create_report();
             }
+            */
+
+            // Keep team reports at window height
+            jQuery(window).bind('resize', function() { 
+                that.fit_to_window(that.reports_accordion);
+            });
+            that.fit_to_window(that.reports_accordion);
         }
+
+        ReportsPage.prototype.fit_to_window = function(view) {
+            var window_height = jQuery(window).height();
+            console.log(window_height);
+            view.height(window_height * 0.87);
+        };
 
         ReportsPage.prototype.show_team_reports = function() {
             this.tab_create_report.removeClass("active");
