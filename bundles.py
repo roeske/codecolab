4b3e344ee889b4fafa1c469a8819c70f8187712c
Tom Dignan <thomas@toptal.com>
2013-08-02 17:17:32 -0400

    Optimize CSS files with Flask-Assets.

--- bundles.py
+++ bundles.py
@@ -32,3 +32,29 @@ def register(app):
                         output="gen/common_js.js")
 
     assets.register("common_js", common_js)
+
+    theme_light_css = Bundle("css/jquery-ui.css",
+                            "css/themes/light/bootstrap.css",
+                            "css/themes/light/app.css",
+                            "css/themes/light/card.css",
+                            "css/themes/light/comments.css",
+                            "css/themes/light/project_manage.css",
+                            "css/themes/light/project_selection.css",
+                            "css/themes/light/custom_tooltip.css",
+                            "css/themes/light/profile.css",
+                            "css/timepicker.css",
+                            "css/common/reports.css",
+                            "css/common/icons.css",
+                            "css/select2.css",
+                            output="gen/theme_light_css.css")
+    assets.register("theme_light_css", theme_light_css)
+
+    common_css = Bundle("css/google_open_sans.css",
+                        "css/common/project_selection.css",
+                        "css/common/office_hours.css",
+                        "css/common/activity.css",
+                        "css/common/members.css",
+                        "css/common/editor.css",
+                        output="gen/common_css.css")
+
+    assets.register("common_css", common_css)
