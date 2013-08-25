8c4fac6d7e68d09f4702517f0b371014c1dc7f82
Tom Dignan <thomas@toptal.com>
2013-08-25 11:52:19 -0400

    Update base url

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ MAIL_FROM = "thomas@toptal.com"
 
 HEROKU_BASE_URL = "http://damp-retreat-1424.herokuapp.com/"
 LOCAL_BASE_URL = "http://localhost:8080/"
-
-BASE_URL = HEROKU_BASE_URL
+BASE_URL = "http://www.codecolab.com/"
+#BASE_URL = HEROKU_BASE_URL
 #BASE_URL = LOCAL_BASE_URL
 
 THUMBNAIL_HEIGHT = 256
