1c70176ed1fe9fb7a9f00fff43d96dc7a44ca601
Tom Dignan <tom@tomdignan.com>
2013-10-12 17:14:22 -0400

    Omit archived cards from the boards.

--- setvars.sh
+++ setvars.sh
@@ -1,5 +1,5 @@
-export AWS_ACCESS_KEY_ID=$(heroku config:get AWS_ACCESS_KEY_ID)
-export DATABASE_URL=$(heroku config:get DATABASE_URL)
-export AWS_SECRET_ACCESS_KEY=$(heroku config:get AWS_SECRET_ACCESS_KEY)
-export S3_BUCKET=$(heroku config:get S3_BUCKET)
+export AWS_ACCESS_KEY_ID=$(heroku config:get --app $1 AWS_ACCESS_KEY_ID)
+export DATABASE_URL=$(heroku config:get --app $1 DATABASE_URL)
+export AWS_SECRET_ACCESS_KEY=$(heroku config:get --app $1 AWS_SECRET_ACCESS_KEY)
+export S3_BUCKET=$(heroku config:get --app $1 S3_BUCKET)
 
