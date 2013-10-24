d653395d52f16b54c1ea3294315f20968316665d
Tom Dignan <tom@tomdignan.com>
2013-10-24 17:06:59 -0400

    WIP

--- bundles.py
+++ bundles.py
@@ -6,7 +6,8 @@ def register(app):
     assets = Environment(app)
     assets.debug = True
 
-    css_overrides = Bundle("css/google_open_sans.css",
+    css_overrides = Bundle("css/jquery-ui.css",
+                           "css/google_open_sans.css",
                            "css/app.css", 
                            output="gen/overrides.css")
 
@@ -17,6 +18,7 @@ def register(app):
                        "js/select2.js",
                        "js/socket.io.js",
                        "js/jquery-ui.js",
+                       "js/timepicker.js",
                         output="gen/boards_js.js") 
 
     assets.register("boards_js", boards_js)
